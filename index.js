c3a5b8a2bc43078ebf30c6d73a7945ce1f59dda1
Youri Daamen <me@youz.io>
2017-07-09 21:59:47 +0200

    yes

--- index.js
+++ index.js
@@ -1,72 +1,35 @@
-const { dirname, join } = require('path')
+const { join, relative } = require('path')
 const fs = require('then-fs')
 const cwd = process.cwd()
 
 // in my node_modules find node_modules recusively and link them all into node_modules if they dont exist
 const rootModulesPath = join(cwd, 'node_modules')
-
-const link = (modulesPath, scopedPkg) => {
-  return fs.access(modulesPath, () => fs.readdir(modulesPath).then(modules => {
-    return Promise.all(modules.map(module => {
-      if (module[0] !== '.' || module === '.bin') {
-        if (module[0] === '@') {
-          return link(join(modulesPath, module), module)
-        } else if (scopedPkg) {
-          const dir = join(rootModulesPath, scopedPkg)
-          return Promise.all([
-            fs.access(dir).catch(() => fs.mkdir(dir)).then(() => {
-              return fs.symlink(join(modulesPath, module), join(dir, module))
-            }),
+const store = {}
+const link = (modulesPath, scopedPkg = '') => fs.access(modulesPath)
+  .then(() => fs.readdir(modulesPath))
+  .then(modules => Promise.all(modules.map(module => {
+    if (module[0] !== '.' || module === '.bin') {
+      const from = join(modulesPath, module)
+      const to = join(rootModulesPath, scopedPkg, module)
+      return module[0] === '@'
+        ? fs.access(to)
+            .catch(() => fs.mkdir(to))
+            .then(() => link(from, module))
+        : Promise.all([
+            to in store ? null : fs.symlink(from, to)
+              .then(() => { store[to] = from })
+              .catch(() => { store[to] = false }),
             link(join(modulesPath, module, 'node_modules'))
           ])
-        } else {
-          return Promise.all([
-            fs.symlink(join(modulesPath, module), join(rootModulesPath, module)),
-            link(join(modulesPath, module, 'node_modules'))
-          ])
-        }
       }
-    }))
-  }))
-}
+    })))
+    .catch(e => e)
 
-link(rootModulesPath).then(() => console.log('DONE YAY')).catch(err => console.log(err))
-
-// fs.readdir(rootModulesPath).then(modules => {
-//   return Promise.all(modules.map(module => {
-//     return link(join(rootModulesPath, module))
-//   }))
-// }).then(() => console.log('DONE YAY')).catch(err => console.log(err))
-
-// const link = (modules, fromDir, toDir) => {
-//   const links = []
-//   return Promise.all(modules.map(name => {
-//     const fromModule = join(fromDir, name)
-//     const toModule = join(toDir, name)
-//     // if its scoped lets go nested, creating dir in the target if nessecary
-//     // else lets link it, it will fail if it's already there, and thats fine
-//     if (name[0] === '@') {
-//       return Promise.all([
-//         fs.access(toModule).catch(() => fs.mkdir(toModule)),
-//         fs.readdir(fromModule)
-//       ]).then(([err, nestedModules]) => link(nestedModules, fromModule, toModule))
-//     } else {
-//       return fs.symlink(fromModule, toModule)
-//         .then(() => links.push(fromModule))
-//         .catch(err => err)
-//     }
-//   })).then(() => console.log('LINKS:', links.join('/n'))).catch(err => console.log('FUUU', err))
-// }
-
-// const modulesPath = join(cwd, 'node_modules')
-
-// // read my modules and check if im a dependency in some other project
-// Promise.all([
-//   fs.readdir(modulesPath),
-//   pkgDir(join(cwd, '..'))
-// ]).then(([modules, target]) => {
-//   if (modules && target) {
-//     // we have modules and im a dep! let link my modules into the dependant
-//     link(modules, modulesPath, join(target, 'node_modules'))
-//   }
-// })
+link(rootModulesPath).then(() => {
+  const lines = Object.keys(store)
+    .filter(to => store[to])
+    .map(to => `${relative(rootModulesPath, to)} -> ${relative(rootModulesPath, store[to])}`)
+  if (lines.length) {
+    console.log('linked nested node_modules:\n' + lines.join('\n'))
+  }
+})
